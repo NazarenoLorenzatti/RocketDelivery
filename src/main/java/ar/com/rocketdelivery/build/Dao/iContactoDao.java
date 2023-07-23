5414d1e011af642638964b79927cb51886d18f0c
dubraska31 <dubraskaprimi31@gmail.com>
2023-07-23 19:01:06 -0300

    nuevo EP @GetMapping("/buscar-contacto/username/{username}")

--- src/main/java/ar/com/rocketdelivery/build/Dao/iContactoDao.java
+++ src/main/java/ar/com/rocketdelivery/build/Dao/iContactoDao.java
@@ -1,12 +1,16 @@
 package ar.com.rocketdelivery.build.Dao;
 
-import ar.com.rocketdelivery.build.domain.reportes.Contacto;
-import ar.com.rocketdelivery.build.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ar.com.rocketdelivery.build.domain.reportes.Contacto;
+import ar.com.rocketdelivery.build.domain.usuario.Usuario;
+
 public interface iContactoDao extends JpaRepository<Contacto, Long> {
-    
-    public Contacto findByNombre(String nombre);
-    
-    public Contacto findByUsuario(Usuario usuario);
+
+	Contacto findByNombre(String nombre);
+
+	Contacto findByUsuario(Usuario usuario);
+
+	Contacto findByUsuarioUsername(String username);
+
 }
--- src/main/java/ar/com/rocketdelivery/build/controller/ApiController.java
+++ src/main/java/ar/com/rocketdelivery/build/controller/ApiController.java
@@ -33,163 +33,166 @@ import ar.com.rocketdelivery.build.service.UsuarioService;
 @RequestMapping("/api")
 public class ApiController {
 
-    // -------------------------------- SERVICIOS ----------------------------------------//
-    @Autowired
-    private MenuService menuService;
-    @Autowired
-    private IngredienteService ingredienteService;
-    @Autowired
-    private PedidoService pedidoService;
-    @Autowired
-    private ContactoService contactoService;
-    @Autowired
-    private UsuarioService usuarioService;
-    // -----------------------------------------------------------------------------------//
+	// -------------------------------- SERVICIOS
+	// ----------------------------------------//
+	@Autowired
+	private MenuService menuService;
+	@Autowired
+	private IngredienteService ingredienteService;
+	@Autowired
+	private PedidoService pedidoService;
+	@Autowired
+	private ContactoService contactoService;
+	@Autowired
+	private UsuarioService usuarioService;
+	// -----------------------------------------------------------------------------------//
 
-    @PostMapping("/crear-contacto")
-    public void crearContacto(@RequestBody Contacto contacto) throws Exception {
-        contactoService.crearContacto(contacto.getNombre(), contacto.getApellido(), contacto.getEmail(),
-                contacto.getTelefono(), contacto.getDireccion(), usuarioService.buscarUsuario(contacto.getUsuario().getIdUsuario()));
-    }
+	@PostMapping("/crear-contacto")
+	public void crearContacto(@RequestBody Contacto contacto) throws Exception {
+		contactoService.crearContacto(contacto.getNombre(), contacto.getApellido(), contacto.getEmail(),
+				contacto.getTelefono(), contacto.getDireccion(),
+				usuarioService.buscarUsuario(contacto.getUsuario().getIdUsuario()));
+	}
 
-    @PostMapping("/crear-ingrediente")
-    public void crearIngrediente(@RequestBody IngredienteStock ingredienteEnStock) {
-        ingredienteService.crearIngredienteStock(ingredienteEnStock.getNombreIngrediente(),
-                ingredienteEnStock.getDescripcionIngrediente(), ingredienteEnStock.getImagenIngrediente(),
-                ingredienteEnStock.getCantidadStock());
-        actualizarStock();
-    }
+	@PostMapping("/crear-ingrediente")
+	public void crearIngrediente(@RequestBody IngredienteStock ingredienteEnStock) {
+		ingredienteService.crearIngredienteStock(ingredienteEnStock.getNombreIngrediente(),
+				ingredienteEnStock.getDescripcionIngrediente(), ingredienteEnStock.getImagenIngrediente(),
+				ingredienteEnStock.getCantidadStock());
+		actualizarStock();
+	}
 
-    @PostMapping("/crear-menu")
-    public void crearMenu(@RequestBody Menu menu) {
-        menuService.crearMenu(menu, menu.getIngredientesEnMenu());
-        actualizarStock();
-    }
+	@PostMapping("/crear-menu")
+	public void crearMenu(@RequestBody Menu menu) {
+		menuService.crearMenu(menu, menu.getIngredientesEnMenu());
+		actualizarStock();
+	}
 
-    @PostMapping("/crear-pedido")
-    public void crearPedido(@RequestBody Pedido pedido) {
-        pedidoService.crearPedido(pedido.getMenus(), contactoService.buscarPorId(pedido.getContacto().getIdContacto()));
-        actualizarStock();
-    }
+	@PostMapping("/crear-pedido")
+	public void crearPedido(@RequestBody Pedido pedido) {
+		pedidoService.crearPedido(pedido.getMenus(), contactoService.buscarPorId(pedido.getContacto().getIdContacto()));
+		actualizarStock();
+	}
 
-    // ENDPOINTS PARA EDITAR 
-    @PostMapping("/editar-ingrediente")
-    public void editarIngrediente(@RequestBody IngredienteStock ingredienteEnStock) {
-        ingredienteService.actualizarIngredienteStock(ingredienteEnStock.getIdIngredienteStock(),
-                ingredienteEnStock.getNombreIngrediente(), ingredienteEnStock.getDescripcionIngrediente(),
-                ingredienteEnStock.getImagenIngrediente());
-        actualizarStock();
-    }
+	// ENDPOINTS PARA EDITAR
+	@PostMapping("/editar-ingrediente")
+	public void editarIngrediente(@RequestBody IngredienteStock ingredienteEnStock) {
+		ingredienteService.actualizarIngredienteStock(ingredienteEnStock.getIdIngredienteStock(),
+				ingredienteEnStock.getNombreIngrediente(), ingredienteEnStock.getDescripcionIngrediente(),
+				ingredienteEnStock.getImagenIngrediente());
+		actualizarStock();
+	}
 
-    @PostMapping("/editar-contacto")
-    public void editarContact(@RequestBody Contacto contacto) throws Exception {
-        contactoService.actualizarContacto(contacto);
-    }
+	@PostMapping("/editar-contacto")
+	public void editarContact(@RequestBody Contacto contacto) throws Exception {
+		contactoService.actualizarContacto(contacto);
+	}
 
-    @PostMapping("/cambiar-contraseña")
-    public void cambiarContraseña(@RequestBody Usuario usuario) throws Exception {
-        usuarioService.cambiarContraseña(usuario.getUsername(), usuario.getPassword());
-    }
+	@PostMapping("/cambiar-contraseña")
+	public void cambiarContraseña(@RequestBody Usuario usuario) throws Exception {
+		usuarioService.cambiarContraseña(usuario.getUsername(), usuario.getPassword());
+	}
 
-    @DeleteMapping("/eliminar-ususario/{usuarioId}")
-    public void eliminarUsuario(@PathVariable("usuarioId") Long usuarioId) {
-        usuarioService.eliminarUsuario(usuarioId);
-    }
+	@DeleteMapping("/eliminar-ususario/{usuarioId}")
+	public void eliminarUsuario(@PathVariable("usuarioId") Long usuarioId) {
+		usuarioService.eliminarUsuario(usuarioId);
+	}
 
-    // ACTUALIZAR CANTIDAD DE INGREDIENTES EN STOCK ----------------------------
-    @PostMapping("/actualizar-cantidad")
-    public void actualizarCantidad(@RequestBody IngredienteStock ingredienteEnStock) {
-        ingredienteService.actualizarCantidadStock(ingredienteEnStock.getIdIngredienteStock(), ingredienteEnStock.getCantidadStock());
-    }
-    
-    //--------------------------------------------------------------------------
-    // ESTADOS DEL PEDIDO
-    @PostMapping("/en-progreso")
-    public void establecerEnProgreso(@RequestBody Pedido pedido) {
-        pedidoService.establecerEnProgreso(pedido);
-    }
+	// ACTUALIZAR CANTIDAD DE INGREDIENTES EN STOCK ----------------------------
+	@PostMapping("/actualizar-cantidad")
+	public void actualizarCantidad(@RequestBody IngredienteStock ingredienteEnStock) {
+		ingredienteService.actualizarCantidadStock(ingredienteEnStock.getIdIngredienteStock(),
+				ingredienteEnStock.getCantidadStock());
+	}
 
-    @PostMapping("/entregado")
-    public void establecerEntregado(@RequestBody Pedido pedido) {
-        pedidoService.establecerEntregado(pedido);
-    }
+	// --------------------------------------------------------------------------
+	// ESTADOS DEL PEDIDO
+	@PostMapping("/en-progreso")
+	public void establecerEnProgreso(@RequestBody Pedido pedido) {
+		pedidoService.establecerEnProgreso(pedido);
+	}
 
-    @PostMapping("/cancelado")
-    public void establecerCancelado(@RequestBody Pedido pedido) {
-        pedidoService.establecerCancelado(pedido);
-    }
+	@PostMapping("/entregado")
+	public void establecerEntregado(@RequestBody Pedido pedido) {
+		pedidoService.establecerEntregado(pedido);
+	}
 
-    @PostMapping("/listo-para-entrega")
-    public void listoParaEntregar(@RequestBody Pedido pedido) {
-        pedidoService.establecerListoParaEntregar(pedido);
-    }
+	@PostMapping("/cancelado")
+	public void establecerCancelado(@RequestBody Pedido pedido) {
+		pedidoService.establecerCancelado(pedido);
+	}
 
-    //--------------------------------------------------------------------------
+	@PostMapping("/listo-para-entrega")
+	public void listoParaEntregar(@RequestBody Pedido pedido) {
+		pedidoService.establecerListoParaEntregar(pedido);
+	}
 
-    // ENPOINTS PARA LISTAR 
+	// --------------------------------------------------------------------------
 
-    @GetMapping("/listar-usuarios")
-    public List<Usuario> getUsuarios() {
-        return usuarioService.listaUsuarios();
-    }
+	// ENPOINTS PARA LISTAR
 
-    
-    @GetMapping("/listar-contactos")
-    public List<Contacto> getContactos() {
-        return contactoService.listarContactos();
-    }
+	@GetMapping("/listar-usuarios")
+	public List<Usuario> getUsuarios() {
+		return usuarioService.listaUsuarios();
+	}
 
-    @GetMapping("/listar-menus")
-    public List<Menu> getMenus() {
-        return menuService.listarMenus();
-    }
+	@GetMapping("/listar-contactos")
+	public List<Contacto> getContactos() {
+		return contactoService.listarContactos();
+	}
 
-    @GetMapping("/listar-pedidos")
-    public List<Pedido> getPedidos() {
-        return pedidoService.listarPedidos();
-    }
+	@GetMapping("/listar-menus")
+	public List<Menu> getMenus() {
+		return menuService.listarMenus();
+	}
 
-    @GetMapping("/listar-stock")
-    public List<IngredienteStock> getStock() {
-        return ingredienteService.listarIngredientesEnStock();
-    }
+	@GetMapping("/listar-pedidos")
+	public List<Pedido> getPedidos() {
+		return pedidoService.listarPedidos();
+	}
 
-    //ACTUALIZAR DISPONIBILIDAD DE MENUS --------------------------
-    @GetMapping("/actualizar-disponibilidad")
-    public void actualizarStock() {
-        menuService.actualizarDisponibilidad();
-    }
+	@GetMapping("/listar-stock")
+	public List<IngredienteStock> getStock() {
+		return ingredienteService.listarIngredientesEnStock();
+	}
 
-    //BUSCAR USUARIO     
-    @GetMapping("/buscar-ususario/{username}")
-    public Usuario eliminarUsuario(@PathVariable("username") String username) {
-        return usuarioService.buscarUsuario(username);
-    }
+	// ACTUALIZAR DISPONIBILIDAD DE MENUS --------------------------
+	@GetMapping("/actualizar-disponibilidad")
+	public void actualizarStock() {
+		menuService.actualizarDisponibilidad();
+	}
 
-    // IMPRESION DE REPORTES
-    @GetMapping("/descargar-reporte")
-    public ResponseEntity<byte[]> descargarReporte() {
+	// BUSCAR USUARIO
+	@GetMapping("/buscar-ususario/{username}")
+	public Usuario buscarUsuario(@PathVariable("username") String username) {
+		return usuarioService.buscarUsuario(username);
+	}
 
-        ByteArrayOutputStream outputStream = pedidoService.reportePedidos();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file-name.xlsx");
+	@GetMapping("/buscar-contacto/username/{username}")
+	public Contacto buscarContactoByUsername(@PathVariable("username") String username) {
+		return contactoService.buscarContactoByUsername(username);
+	}
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(outputStream.toByteArray());
-    }
+	// IMPRESION DE REPORTES
+	@GetMapping("/descargar-reporte")
+	public ResponseEntity<byte[]> descargarReporte() {
 
-    @GetMapping("/descargar-cartera")
-    public ResponseEntity<byte[]> descargarCartera() {
+		ByteArrayOutputStream outputStream = pedidoService.reportePedidos();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file-name.xlsx");
 
-        ByteArrayOutputStream outputStream = contactoService.reporteClientes();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file-name.xlsx");
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(outputStream.toByteArray());
+	}
 
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(outputStream.toByteArray());
-    }
+	@GetMapping("/descargar-cartera")
+	public ResponseEntity<byte[]> descargarCartera() {
+
+		ByteArrayOutputStream outputStream = contactoService.reporteClientes();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file-name.xlsx");
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(outputStream.toByteArray());
+	}
 }
